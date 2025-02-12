4ae04dce9fd0358d7754f0bd5ea8ae34cee72b9f
alvalea <alvalea@gmail.com>
2025-02-12 11:54:53 +0100

    Add Weak example

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use std::cell::RefCell;
 use std::option::Option;
-use std::rc::Rc;
+use std::rc::{Rc, Weak};
 
-pub struct StackNode<T: std::fmt::Display> {
+struct StackNode<T: std::fmt::Display> {
     value: T,
     next: Option<Box<StackNode<T>>>,
 }
 
 impl<T: std::fmt::Display> StackNode<T> {
-    pub fn new(value: T) -> Box<Self> {
+    fn new(value: T) -> Box<Self> {
         Box::new(StackNode { value, next: None })
     }
 }
@@ -19,16 +19,16 @@ impl<T: std::fmt::Display> Drop for StackNode<T> {
     }
 }
 
-pub struct Stack<T: std::fmt::Display> {
+struct Stack<T: std::fmt::Display> {
     top: Option<Box<StackNode<T>>>,
 }
 
 impl<T: std::fmt::Display> Stack<T> {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self { top: None }
     }
 
-    pub fn push(&mut self, value: T) {
+    fn push(&mut self, value: T) {
         let mut new_node = StackNode::new(value);
         match self.top.take() {
             Some(old_top) => {
@@ -39,7 +39,7 @@ impl<T: std::fmt::Display> Stack<T> {
         self.top = Some(new_node);
     }
 
-    pub fn print(&self) {
+    fn print(&self) {
         let mut node = &self.top;
         while let Some(current) = node {
             println!("{}", current.value);
@@ -48,7 +48,7 @@ impl<T: std::fmt::Display> Stack<T> {
     }
 }
 
-pub struct ListNode<T: std::fmt::Display> {
+struct ListNode<T: std::fmt::Display> {
     value: T,
     next: Option<Rc<RefCell<ListNode<T>>>>,
 }
@@ -65,20 +65,20 @@ impl<T: std::fmt::Display> Drop for ListNode<T> {
     }
 }
 
-pub struct List<T: std::fmt::Display> {
+struct List<T: std::fmt::Display> {
     head: Option<Rc<RefCell<ListNode<T>>>>,
     tail: Option<Rc<RefCell<ListNode<T>>>>,
 }
 
 impl<T: std::fmt::Display> List<T> {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self {
             head: None,
             tail: None,
         }
     }
 
-    pub fn insert(&mut self, value: T) {
+    fn insert(&mut self, value: T) {
         let node = ListNode::new(value);
         if let Some(old_tail) = self.tail.take() {
             old_tail.borrow_mut().next = Some(node.clone());
@@ -119,7 +119,7 @@ impl<T: std::fmt::Display> List<T> {
         }
     }
 
-    pub fn delete(&mut self, index: usize) {
+    fn delete(&mut self, index: usize) {
         if index == 0 {
             self.delete_zero();
         } else {
@@ -127,11 +127,136 @@ impl<T: std::fmt::Display> List<T> {
         }
     }
 
-    pub fn print(&self) {
+    fn print(&self) {
         let mut node = self.head.clone();
         while let Some(current) = node {
             println!("{} -> ", current.borrow().value);
             node = current.borrow().next.clone();
+        }
+    }
+}
+
+struct TreeNode<T: std::fmt::Display> {
+    value: T,
+    parent: Weak<RefCell<TreeNode<T>>>,
+    left: Option<Rc<RefCell<TreeNode<T>>>>,
+    right: Option<Rc<RefCell<TreeNode<T>>>>,
+}
+
+impl<T: std::fmt::Display> TreeNode<T> {
+    fn new(value: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            value,
+            parent: Weak::new(),
+            left: None,
+            right: None,
+        }))
+    }
+
+    fn insert(node: &Rc<RefCell<Self>>, value: T)
+    where
+        T: PartialOrd,
+    {
+        let mut node_mut = node.borrow_mut();
+        if value < node_mut.value {
+            match &node_mut.left {
+                None => {
+                    let new_node = TreeNode::new(value);
+                    new_node.borrow_mut().parent = Rc::downgrade(node);
+                    node_mut.left = Some(new_node);
+                }
+                Some(left_node) => TreeNode::insert(left_node, value),
+            }
+        } else {
+            match &node_mut.right {
+                None => {
+                    let new_node = TreeNode::new(value);
+                    new_node.borrow_mut().parent = Rc::downgrade(node);
+                    node_mut.right = Some(new_node);
+                }
+                Some(right_node) => TreeNode::insert(right_node, value),
+            }
+        }
+    }
+
+    fn delete(node: &Rc<RefCell<Self>>, value: T) -> bool
+    where
+        T: PartialOrd,
+    {
+        let mut node_mut = node.borrow_mut();
+        if node_mut.value == value {
+            node_mut.left = None;
+            node_mut.right = None;
+            return true;
+        } else if node_mut.value > value {
+            if let Some(left_node) = &node_mut.left {
+                if TreeNode::delete(left_node, value) {
+                    node_mut.left = None;
+                }
+            }
+        } else if node_mut.value < value {
+            if let Some(right_node) = &node_mut.right {
+                if TreeNode::delete(right_node, value) {
+                    node_mut.right = None;
+                }
+            }
+        }
+        return false;
+    }
+
+    fn print(node: &Rc<RefCell<Self>>) {
+        let current_node = node.borrow();
+        println!("{} ", current_node.value);
+        if let Some(left_node) = &current_node.left {
+            TreeNode::print(left_node);
+        }
+        if let Some(right_node) = &current_node.right {
+            TreeNode::print(right_node);
+        }
+    }
+}
+
+impl<T: std::fmt::Display> Drop for TreeNode<T> {
+    fn drop(&mut self) {
+        println!("Drop treenode {}", self.value);
+    }
+}
+
+struct Tree<T: std::fmt::Display> {
+    root: Option<Rc<RefCell<TreeNode<T>>>>,
+}
+
+impl<T: std::fmt::Display> Tree<T> {
+    fn new() -> Self {
+        Self { root: None }
+    }
+
+    fn insert(&mut self, value: T)
+    where
+        T: PartialOrd,
+    {
+        match &self.root {
+            None => self.root = Some(TreeNode::new(value)),
+            Some(root_node) => TreeNode::insert(root_node, value),
+        }
+    }
+
+    fn delete(&mut self, value: T)
+    where
+        T: PartialOrd,
+    {
+        if let Some(root_node) = &self.root {
+            if root_node.borrow().value == value {
+                self.root = None;
+            } else {
+                TreeNode::delete(root_node, value);
+            }
+        }
+    }
+
+    fn print(&self) {
+        if let Some(root_node) = &self.root {
+            TreeNode::print(root_node)
         }
     }
 }
@@ -155,8 +280,26 @@ fn test_list() {
     list.print();
 }
 
+fn test_tree() {
+    let mut tree = Tree::new();
+    tree.insert(5);
+    tree.insert(3);
+    tree.insert(7);
+    tree.insert(1);
+    tree.insert(4);
+    tree.insert(6);
+    tree.insert(8);
+    tree.print();
+    println!("Before delete\n");
+    tree.delete(3);
+    println!("After delete\n");
+    tree.print();
+}
+
 fn main() {
     test_stack();
     println!("\n");
     test_list();
+    println!("\n");
+    test_tree();
 }
